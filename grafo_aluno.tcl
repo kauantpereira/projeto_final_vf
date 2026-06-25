954dab775e8d457e203c135b25d410f4e38606b8
kauantpereira <kauanteixeirapereira5@gmail.com>
2026-06-25 20:21:25 -0300

    Complete Tarefa 3: FSM graph and BFS min_transicoes

--- grafo_aluno.tcl
+++ grafo_aluno.tcl
@@ -27,11 +27,11 @@ puts "============================================================\n"
 #
 # >>> COMPLETE AQUI <<<
 array set grafo {
-    S_IDLE     {}
-    S_MEM_REQ  {}
-    S_WAIT_ACK {}
-    S_RESP     {}
-    S_ERROR    {}
+    S_IDLE     {S_MEM_REQ}
+    S_MEM_REQ  {S_WAIT_ACK}
+    S_WAIT_ACK {S_RESP S_ERROR}
+    S_RESP     {S_IDLE}
+    S_ERROR    {S_RESP}
 }
 
 
@@ -63,7 +63,25 @@ array set grafo {
 proc min_transicoes {nome_grafo origem destino} {
     upvar 1 $nome_grafo grafo
 
-    # >>> COMPLETE AQUI <<<
+    if {$origem eq $destino} { return 0 }
+
+    set fila [list [list $origem 0]]
+    set visitados [list $origem]
+
+    while {[llength $fila] > 0} {
+        set par   [lindex $fila 0]
+        set fila  [lrange $fila 1 end]
+        set atual [lindex $par 0]
+        set dist  [lindex $par 1]
+
+        foreach vizinho $grafo($atual) {
+            if {$vizinho eq $destino} { return [expr {$dist + 1}] }
+            if {$vizinho ni $visitados} {
+                lappend visitados $vizinho
+                lappend fila [list $vizinho [expr {$dist + 1}]]
+            }
+        }
+    }
 
     return -1
 }
